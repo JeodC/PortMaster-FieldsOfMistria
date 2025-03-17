0021dee4e4523e59189cec7792cb0af51c93849a
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-17 16:48:38 -0400

    Update Fields of Mistria.sh

--- Fields of Mistria.sh
+++ Fields of Mistria.sh
@@ -13,35 +13,37 @@ source $controlfolder/control.txt
 get_controls
 
 # Variables
-GAMEDIR="/$directory/ports/fieldsofmistria"
+GAMEDIR="/$directory/windows/fieldsofmistria"
+EXEC="FieldsOfMistria.exe"
 
-# CD and set permissions
+# CD and set log
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 # Display loading splash
-if [ -f "$GAMEDIR/patchlog.txt" ]; then
-    $ESUDO $GAMEDIR/tools/splash "$GAMEDIR/splash.png" 30000 & 
-fi
+$ESUDO $GAMEDIR/splash "$GAMEDIR/splash.png" 30000 & 
 
-# Exports
 export PATCHER_FILE="$GAMEDIR/tools/patchscript"
 export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
 export PATCHER_TIME="1 to 3 minutes"
-
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export WINEPREFIX=/storage/.wine64
 export WINEDEBUG=-all
+
+# Determine architecture
+if file "$GAMEDIR/data/$EXEC" | grep -q "PE32" && ! file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEARCH=win32
+    export WINEPREFIX=~/.wine32
+elif file "$GAMEDIR/data/$EXEC" | grep -q "PE32+"; then
+    export WINEPREFIX=~/.wine64
+else
+    echo "Unknown file format"
+fi
 
 # Install dependencies
 if ! winetricks list-installed | grep -q "^vcrun2022$"; then
     pm_message "vcrun2022 is not installed. Installing now."
     winetricks --unattended --no-isolate vcrun2022
 fi
-
-# Config Setup
-mkdir -p $GAMEDIR/config
-bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/FieldsOfMistria" "$GAMEDIR/config"
 
 # Check if patchlog.txt to skip patching
 if [ ! -f patchlog.txt ]; then
@@ -53,9 +55,13 @@ if [ ! -f patchlog.txt ]; then
     fi
 fi
 
+# Config Setup
+mkdir -p $GAMEDIR/config
+bind_directories "$WINEPREFIX/drive_c/users/root/AppData/Local/FieldsOfMistria" "$GAMEDIR/config"
+
 # Run the game
-$GPTOKEYB "FieldsOfMistria.exe" -c "./mistria.gptk" &
-$GAMEDIR/box64 wine64 "./data/FieldsOfMistria.exe"
+$GPTOKEYB "$EXEC" -c "$GAMEDIR/mistria.gptk" &
+box64 wine "$GAMEDIR/data/$EXEC"
 
 # Kill processes
 wineserver -k
